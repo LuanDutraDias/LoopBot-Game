9652681562a5ff94cbf0b303e0c03bc8f7b740aa
LuanDutraDias <ldasilvadutradias@gmail.com>
2026-02-10 11:58:58 -0300

    Fixing 'P1' and 'P2' strings' spacing on display

--- index.js
+++ index.js
@@ -409,9 +409,9 @@ function transformCommands(command){
         case 'jump':
             return '<i class="bi bi-capslock-fill"></i>';    
         case 'p1':
-            return '<span>P1<span>'; 
+            return '<span>P1</span>'; 
         case 'p2':
-            return '<span>P2<span>';
+            return '<span>P2</span>';
         case 'main':
             return '<span>MAIN</span>';
         default:
